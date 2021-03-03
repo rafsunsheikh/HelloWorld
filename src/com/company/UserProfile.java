38afd851834cc6a9f968a4c7ddac76b7f2e698da
rafsunsheikh <rafsun.sheikh@gmail.com>
2021-03-03 17:23:12 +0600

    Added database connection and read from db and convert to json

--- src/com/company/UserProfile.java
+++ src/com/company/UserProfile.java
@@ -1,4 +1,5 @@
 /*
+ * This file is demo purpose
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
